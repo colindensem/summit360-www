a5423154f2f6325d27ca11c75064769950949d4a
Colin Densem <colin@summit360.co.uk>
2017-06-04 20:29:44 +0100

    CI updates

--- config.rb
+++ config.rb
@@ -23,7 +23,7 @@ page "/articles/*", :layout => "article"
 page "/articles/", layout: "page"
 page "/", layout: 'layout'
 
-set :url_root, "https://summit360.co.uk" 
+set :url_root, "https://www.summit360.co.uk" 
 
 activate :search_engine_sitemap
 
@@ -70,15 +70,12 @@ activate :blog do |blog|
 end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
 #
 ## Middleman v4 Sprockets
 #
-
 activate :sprockets
 sprockets.append_path 'node_modules/bootstrap-sass/assets/fonts'
 sprockets.append_path 'node_modules/bootstrap-sass/assets/javascripts'
@@ -87,35 +84,26 @@ sprockets.append_path 'node_modules'
 ## DEPLOY
 #
 activate :deploy do |deploy|
-
   deploy.deploy_method = :git
   deploy.remote = 'github'
   deploy.branch = 'master'
   deploy.build_before = true
-
 end
 
 # Build-specific configuration
 configure :build do
-
   set :build_dir, 'public'
   #set :base_url, "/middleman" # baseurl for GitLab Pages (project name) - leave empty if you're building a user/group website
-
   # For example, change the Compass output style for deployment
   activate :minify_css
-
   # Minify Javascript on build
   activate :minify_javascript
-
   # Enable cache buster
   activate :asset_hash
-
   # Use relative URLs
   activate :relative_assets
-
   # Minify HTML on build
   activate :minify_html
-
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 
